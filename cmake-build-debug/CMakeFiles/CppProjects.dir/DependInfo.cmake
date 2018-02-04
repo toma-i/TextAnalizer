aabac287d3ef0bad981431f99bfb0257c7e130c1
toma-i <yvan312@gmail.com>
2018-02-04 23:16:27 +0100

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/CppProjects.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "B:/Developpement/CppProjects/main.cpp" "B:/Developpement/CppProjects/cmake-build-debug/CMakeFiles/CppProjects.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "B:/Developpement/Libs/SFML/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CppProjects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppProjects.dir/main.cpp.obj"
+  "CppProjects.pdb"
+  "CppProjects.exe"
+  "CppProjects.exe.manifest"
+  "libCppProjects.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppProjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
